d553bf955c2956995d9c3900f726cad9b4cab820
EpicBanana20 <101955915+EpicBanana20@users.noreply.github.com>
2025-11-23 15:50:48 -0700

    Procesos

--- src/Proceso.java
+++ src/Proceso.java
@@ -1,4 +1,35 @@
 public class Proceso {
-    
+
+    public enum Estado {
+        NUEVO,
+        LISTO,
+        EJECUTANDO,
+        SUSPENDIDO,
+        TERMINADO
+    }
+
+    private int id;
+    private Estado estado;
+
+    public Proceso(int id) {
+        this.id = id;
+        this.estado = Estado.NUEVO; // Estado inicial
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Estado nuevoEstado) {
+        this.estado = nuevoEstado;
+    }
+
+    @Override
+    public String toString() {
+        return "Proceso { ID = " + id + ", Estado = " + estado + " }";
+    }
 }
-//puto
